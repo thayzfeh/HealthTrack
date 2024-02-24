b2dff2770acd2956b62247f3e34eb9b2d5923577
Matheus Luna <115325977+MatheusMLNascimento@users.noreply.github.com>
2024-02-24 13:17:25 -0300

    Importando versões do app
    Se quiser rodar no seu VS Code, ou outra aplicação, basta criar um novo projeto com nome "login_tcc" e substituir os arquivos pubspec.yaml, lib e images

---
+++ HealthTrack_app/login_tcc (v1)/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:login_tcc/pages/cadastro_page.dart';
+import 'package:login_tcc/pages/splash_screen_page.dart';
+import 'package:login_tcc/pages/login_page.dart';
+
+void main() => runApp(const TccHome());
+
+class TccHome extends StatelessWidget {
+  const TccHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const SplashScreenPage(),
+          '/login': (context) => const LoginPage(),
+          '/cadastro': (context) => const CadastroPage()
+        });
+  }
+}
