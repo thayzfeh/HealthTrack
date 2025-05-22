e7b8d02898758b8325335b7e6da1bfdd8604acd7
matheusmlnascimento <martinsmatheus986@gmail.com>
2025-05-21 21:36:09 -0300

    fixed the app crashing on init

--- login_tcc/android/app/build.gradle.kts
+++ login_tcc/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.healthtrack.app"
+    namespace = "com.logintcc.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.app"
+        applicationId = "com.example.login_tcc"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
